8abd8470c35039c08ede6498d16cbd6fc6a9e062
ellen.schwartau.orbit <ellen@orbitdigital.de>
2020-11-17 15:30:48 +0100

    basic spring boot kotlin application

---
+++ src/main/kotlin/de/ellen/demo/DemoApplication.kt
@@ -0,0 +1,12 @@
+package de.ellen.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
